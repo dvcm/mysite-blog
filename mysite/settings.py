d1d39d105d1ba4a19910f3d3b3055e96493368e4
dvcm <pshaggy@gmail.com>
2023-02-28 17:26:25 -0500

    28-02-2023

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,8 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIRS = os.path.join(BASE_DIR, "../templates")
 
-TEMPLATES_DIRS = os.path.join(BASE_DIR, "templates")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-!$7)w04b9n(f29=ao_1c@m&=@y-g&ruflms$%go#t%hy61ct!8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
